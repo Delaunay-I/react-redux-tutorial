1ab03b67fff99666d7380b2c7d548b4853332468
Amirhossein Mirshahi <amirmsh@student.ubc.ca>
2023-11-30 19:10:09 -0800

    added increase and decrease actions

--- src/features/cart/cartSlice.js
+++ src/features/cart/cartSlice.js
@@ -3,7 +3,7 @@ import cartItems from "../../cartItems";
 
 const initialState = {
   cartItems: cartItems,
-  amount: 8,
+  amount: 2, //total Items in the cart
   total: 0,
   isLoading: true,
 };
@@ -15,9 +15,21 @@ const cartSlice = createSlice({
     clearCart: (state) => {
       state.cartItems = [];
     },
+    removeItem: (state, action) => {
+      const itemId = action.payload;
+      state.cartItems = state.cartItems.filter((item) => item.id !== itemId);
+    },
+    increase: (state, { payload }) => {
+      const cartItem = state.cartItems.find((item) => item.id === payload.id);
+      cartItem.amount = cartItem.amount + 1;
+    },
+    decrease: (state, { payload }) => {
+      const cartItem = state.cartItems.find((item) => item.id === payload.id);
+      cartItem.amount = cartItem.amount - 1;
+    },
   },
 });
 
 // console.log(cartSlice);
-export const { clearCart } = cartSlice.actions;
+export const { clearCart, removeItem, increase, decrease } = cartSlice.actions;
 export default cartSlice.reducer;
