5cf9f42aab88b5ad640026350fd8ca97f27d3111
Amirhossein Mirshahi <amirmsh@student.ubc.ca>
2023-11-30 19:18:55 -0800

    Added total calculation for amount and price fix decimal precision for total price

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,17 @@
+import { useSelector, useDispatch } from "react-redux";
+import { useEffect } from "react";
+
+import { calculateTotals } from "./features/cart/cartSlice";
 import Navbar from "./components/Navbar";
 import CartContainer from "./components/CartContainer";
 
-
 function App() {
+  const { cartItems } = useSelector((store) => store.cart);
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    dispatch(calculateTotals());
+  }, [cartItems]);
   return (
     <main>
       <Navbar />
